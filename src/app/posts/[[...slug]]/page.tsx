570f82cfaa6a9bf00f46f1ad471ad053b77dc8e6
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-10-19 00:04:54 +0700

    fix: useState

--- src/app/posts/[[...slug]]/page.tsx
+++ src/app/posts/[[...slug]]/page.tsx
@@ -26,7 +26,7 @@ export default function Page(slug: any) {
     }
     useEffect(() => {
         fetchData()
-    }, [data])
+    }, [fetchData])
 
     return (
         <div className={clsx('flex flex-col w-full')}>
